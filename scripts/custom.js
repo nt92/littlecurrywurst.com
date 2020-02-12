38c2533a0cb88443c2afc14d6978a3dd8506789d
Nikhil Thota <nikhilthota@fb.com>
2020-02-12 11:49:10 -0800

    safari fun

--- scripts/custom.js
+++ scripts/custom.js
@@ -3,3 +3,11 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-151422011-1');
+
+function safariBullshit() {
+    if(document.getElementById('index-body').classList.contains('safari-margin')) {
+        document.getElementById('index-body').classList.remove('safari-margin');
+    } else {
+        document.getElementById('index-body').classList.add('safari-margin');
+    }
+}
